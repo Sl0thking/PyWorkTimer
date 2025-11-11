bcb0cf5f646a8381b3bf2e7a632695502aeade6e
Slothy <k.jolitz-dev@outlook.de>
2025-11-11 07:19:53 +0100

    Init

---
+++ timer.py
@@ -0,0 +1,116 @@
+import json
+from datetime import datetime, timedelta
+import time
+
+
+class WorkTimer:
+    def __init__(self):
+        self.work_timer = Timer()
+        self.pause_timer = Timer()
+        self.work_times = {}
+        self.curr_date = None
+
+    def track_day(self, date=None):
+        if self.curr_date and self.curr_date == date:
+            return
+
+        if self.work_timer.duration() < timedelta(0):
+            self.work_times[date]["work"] += self.work_timer.duration()
+
+        if self.pause_timer.duration() < timedelta(0):
+            self.work_times[date]["pause"] += self.pause_timer.duration()
+
+        self.curr_date = date
+        if self.curr_date not in self.work_times:
+            self.work_times[date] = {
+                "pause": timedelta(0),
+                "work": timedelta(0)
+            }
+
+        self.work_timer.reset()
+        self.pause_timer.reset()
+
+    def start(self):
+        self.work_timer.start()
+
+    def stop(self):
+        self.work_timer.stop()
+        self.refresh()
+
+    def pause(self):
+        self.pause_timer.start()
+
+    def unpause(self):
+        self.pause_timer.stop()
+        self.refresh()
+
+    def refresh(self):
+        self.work_times[self.curr_date]["work"] = self.work_timer.duration()
+        self.work_times[self.curr_date]["pause"] = self.pause_timer.duration()
+
+    def worktime(self, day):
+        if day not in self.work_times:
+            return timedelta(0)
+        self.refresh()
+        return self.work_times[day]["work"] - self.work_times[day]["pause"]
+
+    def pausetime(self, day):
+        if day not in self.work_times:
+            return timedelta(0)
+        self.refresh()
+        return self.work_times[day]["pause"]
+
+    def to_json(self):
+        decoded_json = {}
+        for date, times in self.work_times.items():
+            decoded_json[date] = {
+                "pause": str(times["pause"]),
+                "work": str(times["work"]),
+            }
+
+        return json.dumps(decoded_json)
+
+class Timer:
+
+    def __init__(self):
+        self.time = timedelta(0)
+        self.start_time = None
+
+    def reset(self):
+        self.time = timedelta(0)
+        self.start_time = None
+
+    def start(self):
+        if self.start_time:
+            return
+
+        self.start_time = datetime.now()
+
+    def stop(self):
+        if not self.start_time:
+            return
+
+        self.time = datetime.now() - self.start_time
+        self.start_time = None
+
+    def duration(self):
+        acc_time = timedelta(0)
+        if self.start_time:
+            acc_time = datetime.now() - self.start_time
+
+        acc_time += self.time
+
+        return acc_time
+
+
+if __name__ == "__main__":
+    pause_timer = Timer()
+    work_timer = Timer()
+    work_timer.start()
+    time.sleep(5)
+    print(work_timer.duration())
+    pause_timer.start()
+    time.sleep(5)
+    pause_timer.stop()
+    work_timer.stop()
+    print(work_timer.duration() - pause_timer.duration())
---
+++ ui.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+from tkinter import Tk, ttk, font
+import subprocess
+
+from timer import Timer, WorkTimer
+
+timer = WorkTimer()
+screen_locked = False
+curr_day = datetime.now().strftime("%d.%m.%Y")
+
+def start():
+    global timer
+    global start_btn
+    global curr_day
+
+    timer.track_day(curr_day)
+    timer.start()
+
+    start_btn["command"] = stop
+    start_btn["text"] = "Stop"
+
+def stop():
+    global timer
+    timer.stop()
+
+    start_btn["command"] = start
+    start_btn["text"] = "Start"
+
+def pause():
+    global timer
+    global pause_btn
+    timer.pause()
+    pause_btn["command"] = unpause
+    pause_btn["text"] = "Unpause"
+
+root = Tk()
+root.title("Time")
+
+root.geometry("360x240")
+
+highlightFont = font.Font(family='Helvetica', name='appHighlightFont', size=24, weight='bold')
+
+label = ttk.Label(root, text="0:00:00", font=highlightFont)
+label.grid(column=0, row=0)
+
+pause_label = ttk.Label(root, text="0:00:00")
+pause_label.grid(column=1, row=0)
+
+start_btn = ttk.Button(root, text="Start", command=start)
+pause_btn = ttk.Button(root, text="Pause", command=pause)
+
+start_btn.grid(column=0, row=2, sticky="W,E")
+pause_btn.grid(column=1, row=2, sticky="W,E")
+
+root.columnconfigure(0, weight=3)
+root.columnconfigure(1, weight=3)
+
+root.rowconfigure(0, weight=3)
+
+
+def unpause():
+    global timer
+    timer.unpause()
+    pause_btn["command"] = pause
+    pause_btn["text"] = "Pause"
+
+def is_screen_locked():
+    process_name = 'LogonUI.exe'
+    callall = 'TASKLIST'
+    outputall = subprocess.check_output(callall)
+    outputstringall = str(outputall)
+    return process_name in outputstringall
+
+def key(event):
+    print(event)
+
+def timer_process():
+    global timer
+    global screen_locked
+    global curr_day
+
+    if screen_locked and not is_screen_locked():
+        unpause()
+        screen_locked = False
+    elif is_screen_locked():
+        pause()
+        screen_locked = True
+
+    label["text"] = str(timer.worktime(curr_day)).split(".")[0]
+    pause_label["text"] = str(timer.pausetime(curr_day)).split(".")[0]
+    root.after(1000, timer_process)
+
+root.after(1000, timer_process)
+root.mainloop()
